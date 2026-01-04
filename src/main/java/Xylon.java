7c5a97c09448af5d78823503ffda042688b2b19f
xylon <101974919+xylonc@users.noreply.github.com>
2026-01-05 02:38:11 +0800

    added small increment of export but not comepleted

--- src/main/java/Xylon.java
+++ src/main/java/Xylon.java
@@ -12,7 +12,7 @@ public class Xylon {
 
         System.out.println("Hello from\n" + logo);
         System.out.println("-".repeat(30));
-        System.out.println("Hello! I'm Xylon");
+        System.out.println("I'm Xylon");
         System.out.println("How may I help you?\n");
         System.out.println("-".repeat(30) + "\n");
     }
